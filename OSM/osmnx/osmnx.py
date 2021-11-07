e0c49a56454b24d72b4e47aee8b4bc783e6c2691
eirene8 <github.fcmb@gmail.com>
2021-11-07 23:49:20 +0100

    Added demonstration for shortest path between two nodes

--- OSM/osmnx/osmnx.py
+++ OSM/osmnx/osmnx.py
@@ -16,9 +16,9 @@ Created on Sun Nov  7 17:19:19 2021
 # %% Setup
 
 import osmnx as ox
-import matplotlib as plt
 import networkx as nx
 from geopy.geocoders import Nominatim
+import random as rnd
 
 # %% Extract Lueneburg & Visualize
 
@@ -30,18 +30,20 @@ from geopy.geocoders import Nominatim
 place_name = "Luneburg, Germany"                            # A.1
 place_name = {"city": "Luneburg", "country": "Germany"}     # A.2  
 
-graph = ox.graph_from_place(place_name)
+graph = ox.graph_from_place(place_name, network_type ="drive")
 
 fig, ax = ox.plot_graph(graph)
-plt.tight_layout()
 
+# %% Option B: By coordinates 
 
-# %% Option B: By coordinates (this example doesn't include arena)
+# This example with city center doesn't include arena.
 
-city_center = (53.248706, 10.407855) # coordinates          
+city_center = (53.248706, 10.407855)
+arena       = (53.248706, 10.407855)
+        
 radius = 5000                        # meters
-graph = ox.graph_from_point(city_center, dist=radius, network_type="drive")
 
+graph = ox.graph_from_point(city_center, dist=radius, network_type="drive")
 fig, ax = ox.plot_graph(graph)
 
 # %% Get coordinates for an address
@@ -50,15 +52,12 @@ geolocator = Nominatim(user_agent="sample app")
 
 data = geolocator.geocode("17 Wichernstraße, Luneburg, Germany")
 
-lat = data.raw.get("lat")
-lat = float(lat)
-lon = data.raw.get("lon")
-lon = float(lon)
+lat = float(data.raw.get("lat"))
+lon = float(data.raw.get("lon"))
 
 # %% GeoDataFrames
 
 nodes, edges = ox.graph_to_gdfs(graph)
-print(edges['highway'].value_counts())
 
 #%% Basic insides
 
@@ -76,18 +75,28 @@ stats_basic = ox.basic_stats(graph, area=graph_area_m, clean_int_tol=15)
 graph = ox.speed.add_edge_speeds(graph)
 graph = ox.speed.add_edge_travel_times(graph)
 
+
 # %% Routing II: Define start & end point
 
 # get the nearest network nodes to two lat/lng points with the distance module
-dest = ox.distance.nearest_nodes(graph, X = 53.272312, Y = 10.427605) # city center
-#dest = ox.distance.nearest_nodes(graph, X= 53.248706, Y= 10.407855) # arena
+dest, dest_dist = ox.nearest_nodes(graph, X = 53.272312, Y = 10.427605, return_dist = True) # city center
+dest, dest_dist = ox.distance.nearest_nodes(graph, X= 53.248706, Y= 10.407855, return_dist = True) # arena
 
-orig = ox.distance.nearest_nodes(graph, X = lat,       Y = lon)
+orig, orig_dist = ox.distance.nearest_nodes(graph, X = lat,       Y = lon, return_dist = True)
 
 print(graph.nodes[dest]['x'])
 print(graph.nodes[orig]['x'])
 
-# somehow the same -> THEY SHOULDN'T OF COURSE!!!
+# somehow the same -> THEY SHOULDN'T BE OF COURSE!!!
+
+#%% Select sample nodes (for demonstration purposes only)
+
+nodes_id = graph.nodes._nodes
+nodes_id = nodes_id.keys()
+nodes_id = list(nodes_id)
+
+orig = nodes_id[rnd.randrange(0,len(nodes_id))]
+dest = nodes_id[rnd.randrange(0,len(nodes_id))]
 
 # %% Routing III: Find shortest path
 
