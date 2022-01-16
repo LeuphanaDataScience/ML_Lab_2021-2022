0fd33d3f13bdb061d2efbaa0e83e3941e1be0f04
Clemens <73638512+samisomu@users.noreply.github.com>
2022-01-16 19:00:06 +0100

    passenger assignment

--- passenger_assignment/passenger_assignment_final.py
+++ passenger_assignment/passenger_assignment_final.py
@@ -10,32 +10,25 @@ from scipy import stats
 import random
 import os
 
+from shapely import geometry
+
 #working directory
 os.getcwd()
 os.chdir('C:\\Users\\cleme\\Documents\\GitHub\\ML_Lab_2021-2022\\passenger_assignment')
 
+
 #%% read bus stop locations
 def load_data(area, plot_graph = "n"):
 
-    bus_stops_loc_all = pd.read_csv("bus_stops_data.csv")
-
-    #reduce to non-duplicate stations bus stops (667 stops)
-    no_duplicates = np.array(pd.read_csv("stops_without_duplicates.csv", sep = ";")).flatten().tolist()
-    bus_stops_loc = bus_stops_loc_all[bus_stops_loc_all["osmid"].isin(no_duplicates)]
-    bus_stops_gdf = gp.GeoDataFrame(bus_stops_loc, geometry=gp.points_from_xy(bus_stops_loc.x, bus_stops_loc.y))
-
-    #hardcode wrong "Markt" coordinates into gdf
-    markt_bus_stop = ox.geocoder.geocode_to_gdf("N415971821", which_result=None, by_osmid=True, buffer_dist=None)["geometry"]
-    markt_index = bus_stops_gdf[bus_stops_gdf['name'] == 'Markt'].index[0]
-    bus_stops_gdf.at[markt_index, 'geometry'] = markt_bus_stop.values[0]
-
- 
     def plot_map(graph):
         fig, ax = ox.plot_graph(graph, show=False, close=False)
         plt.show()
 
     if area == "county":
-        bus_stops_geom = bus_stops_gdf['geometry']
+        bus_stops_gdf = pd.read_csv('county_bus_stops.csv', index_col=False)
+        bus_stops_geom = gp.read_file('county_bus_stops.shp')['geometry']
+        bus_stops_gdf = gp.GeoDataFrame(bus_stops_gdf, geometry = bus_stops_geom)
+        bus_stops_gdf = bus_stops_gdf.drop('Unnamed: 0', axis = 1)
         # get area of county Lüneburg and create "drive"-type network
         graph_lüneburg_gdf = ox.geocoder.geocode_to_gdf("R2084746", which_result=None, by_osmid=True, buffer_dist=None)
         graph_lüneburg_geom = graph_lüneburg_gdf['geometry'].iloc[0]
@@ -46,6 +39,17 @@ def load_data(area, plot_graph = "n"):
 
     if area == "city":
         # get area of city of Lüneburg and create "drive"-type network
+        bus_stops_loc_all = pd.read_csv("bus_stops_data.csv")
+
+        #reduce to non-duplicate stations bus stops (667 stops)
+        no_duplicates = np.array(pd.read_csv("stops_without_duplicates.csv", sep = ";")).flatten().tolist()
+        bus_stops_loc = bus_stops_loc_all[bus_stops_loc_all["osmid"].isin(no_duplicates)]
+        bus_stops_gdf = gp.GeoDataFrame(bus_stops_loc, geometry=gp.points_from_xy(bus_stops_loc.x, bus_stops_loc.y))
+        bus_stops_gdf = bus_stops_gdf.drop(['x', 'y'], axis = 1)
+        #hardcode wrong "Markt" coordinates into gdf
+        markt_bus_stop = ox.geocoder.geocode_to_gdf("N415971821", which_result=None, by_osmid=True, buffer_dist=None)["geometry"]
+        markt_index = bus_stops_gdf[bus_stops_gdf['name'] == 'Markt'].index[0]
+        bus_stops_gdf.at[markt_index, 'geometry'] = markt_bus_stop.values[0]
         sparse_matrix = pd.read_csv("sparse_matrix_lueneburg.csv", sep = ";")
         bus_stops_gdf = bus_stops_gdf[bus_stops_gdf["name"].isin(sparse_matrix["name"])]
         bus_stops_geom = bus_stops_gdf['geometry']
@@ -56,14 +60,15 @@ def load_data(area, plot_graph = "n"):
             plot_map(graph)
         return [bus_stops_gdf, bus_stops_geom, graph]
 
+
 data = load_data("county", "n")
 data_city = load_data("city")
 
 #%% sample passengers for all the bus stops
 
-def sample_passengers(data_city, area = "county", mu = 3000, sigma = 500, seed = 0, lower = 0, upper = 3500, empty_stops = 0):
+def sample_passengers(data, area = "county", mu = 3500, sigma = 0.001, seed = 2, lower = 0, upper = 3500, empty_stops = 0.25):
     
-    bus_stops_gdf, bus_stops_geom, graph = data_city
+    bus_stops_gdf, bus_stops_geom, graph = data
 
     #exclude arena and depot from assignment
     arena_index = bus_stops_gdf[bus_stops_gdf["name"] == "Schlachthof"].index[0]
@@ -78,7 +83,8 @@ def sample_passengers(data_city, area = "county", mu = 3000, sigma = 500, seed =
 
     #read district data
     if area == "county":
-        pop = np.array(district_data["pop"])
+        pop = np.array(district_data["pop"][0:26]) #ignore Amt Neuhaus
+        district_data = district_data[0:26]        #ignore Amt Neuhaus
 
     if area == "city":
         pop = np.array(district_data["pop"][0:17])
@@ -99,11 +105,17 @@ def sample_passengers(data_city, area = "county", mu = 3000, sigma = 500, seed =
 
     #check if bus stop lie within district
     for i,id in enumerate(district_data["osm_id"]):
-        #id = "R4191677"
-        #i = 0
+ 
         district_gdf = ox.geocoder.geocode_to_gdf(id, which_result=None, by_osmid=True, buffer_dist=None)
+ 
         district_polygon = district_gdf['geometry'].iloc[0]
         is_in_dis = [district_polygon.contains(j) for j in bus_stops_geom]
+
+        if any(is_in_dis) == False:
+            print('empty')
+            print(visitors_per_district[i])
+            print(district_data["district_name"][i])
+            continue
         stops_in_dis = bus_stops_geom[is_in_dis]
 
         #distribute passengers per district evenly among bus stops, but optiopnally have % of bus stops without passengers
@@ -120,14 +132,15 @@ def sample_passengers(data_city, area = "county", mu = 3000, sigma = 500, seed =
     #add new column to dataframe with passenger numbers
     bus_stops_gdf = bus_stops_gdf.assign(passengers = all_selected_stops)
     arena_depot_stops = arena_depot_stops.assign(passengers = np.zeros(2, dtype = int))
-    bus_stops_gdf = pd.concat([bus_stops_gdf, arena_depot_stops])
+    bus_stops_gdf = pd.concat([bus_stops_gdf, arena_depot_stops]).sort_index()
 
     return bus_stops_gdf
 
 
 gdf_with_pass = sample_passengers(data)
+sum(gdf_with_pass['passengers'])
 
-pd.DataFrame(gdf_with_pass).to_csv("passengers_667_stations.csv")
+pd.DataFrame(gdf_with_pass).to_csv("scenario_538_stations_3.csv")
 
 #%% plot heat map of passengers numbers at stops
 
